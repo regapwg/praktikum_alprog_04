e2e47e7258319c472703dbf17e8ff54659724e8b
Nata Nara Narendra Putra Suanda <96335696+NataNara30@users.noreply.github.com>
2022-03-28 11:26:31 +0800

    Update program 3_bangun ruang.c

--- modul 1/program 3_bangun ruang.c
+++ modul 1/program 3_bangun ruang.c
@@ -19,23 +19,15 @@ float volume_bola();
 float volume_limas_segiempat();
 float volume_prisma_segitiga();
 float volume_kerucut();
-float rumus_luas_permukaan_tabung(float r,float t);
-float rumus_luas_permukaan_bola(float r);
-float rumus_luas_permukaan_limas_segiempat(float sisi, float t);
-float rumus_luas_permukaan_prisma_segitiga(float a, float t, float T_prisma);
-float rumus_luas_permukaan_kerucut(float r, float t);
-float rumus_volume_tabung(float r, float t);
-float rumus_volume_bola(float r);
-float rumus_volume_limas_segiempat(float sisi, float t);
-float rumus_volume_prisma_segitiga(float a, float t, float T_prisma);
-float rumus_volume_kerucut(float r, float t);
 double check();
 int keluar();
 
 
 int main(){
-	menu_awal();
+	
 	judul();
+	menu_awal();
+
 }
 
 int menu_awal(){
@@ -84,7 +76,7 @@ int menu_awal(){
 		printf("=================================================\n");	
 		getch();
 		system("cls");
-		main();
+		menu_awal();
 	}
 }
 
@@ -131,7 +123,7 @@ int menu_tabung(){
 		volume_tabung();
 	}
 	else if(opsi==3){
-		main();
+		menu_awal();
 	}
 	else if(opsi==4){
 		system("cls");
@@ -161,11 +153,11 @@ float luas_permukaan_tabung(){
 	r=check();
 	printf("| Masukkan tinggi    : ");
 	t=check();
-	luas= rumus_luas_permukaan_tabung(r,t);
+	luas = 2*phi*r*(r+t);
 	printf("=================================================\n");
 	printf("| luas permukaan tabung: %.2f \n", luas);
 	printf("=================================================\n");
-	printf("| 1. Input menghitung lagi? \n");
+	printf("| 1. Ingin menghitung lagi? \n");
 	printf("| 2. Keluar\n");
 	printf("=================================================\n");
 	printf("| Masukkan pilihan anda: ");
@@ -181,11 +173,11 @@ float luas_permukaan_tabung(){
 	else {
 		system("cls");
 		printf("=================================================\n");		
-		printf("|            Masukkan input yang benar!         |\n");
+		printf("|          Program Kembali Ke Menu Awal         |\n");
 		printf("=================================================\n");	
 		getch();
 		system("cls");
-		main();
+		menu_awal();
 	}
 	return 0;
 }
@@ -199,11 +191,11 @@ float volume_tabung(){
 	r=check();
 	printf("| Masukkan tinggi    : ");
 	t=check();
-	volume= rumus_volume_tabung(r,t);
+	volume = phi*r*r*t;
 	printf("=================================================\n");
 	printf("| volume tabung: %.2f \n", volume);
 	printf("=================================================\n");
-	printf("| 1. Input menghitung lagi? \n");
+	printf("| 1. Ingin menghitung lagi? \n");
 	printf("| 2. Keluar\n");
 	printf("=================================================\n");
 	printf("| Masukkan pilihan anda: ");
@@ -219,27 +211,13 @@ float volume_tabung(){
 	else {
 		system("cls");
 		printf("=================================================\n");		
-		printf("|            Masukkan input yang benar!         |\n");
+		printf("|          Program Kembali Ke Menu Awal         |\n");
 		printf("=================================================\n");	
 		getch();
 		system("cls");
-		main();
+		menu_awal();
 	}
 	return 0;
-}
-
-float rumus_luas_permukaan_tabung(float r, float t){
-	
-	float luas;
-	luas = 2*phi*r*(r+t);
-	return luas;
-}
-
-float rumus_volume_tabung(float r, float t){
-	
-	float volume;
-	volume = phi*r*r*t;
-	return volume;
 }
 
 
@@ -266,7 +244,7 @@ int menu_bola(){
 		volume_bola();
 	}
 	else if(opsi==3){
-		main();
+		menu_awal();
 	}
 	else if(opsi==4){
 		system("cls");
@@ -294,11 +272,11 @@ float luas_permukaan_bola(){
 	printf("=================================================\n");
 	printf("| Masukkan jari-jari : ");
 	r=check();
-	luas= rumus_luas_permukaan_bola(r);
+	luas= 4*phi*r*r;
 	printf("=================================================\n");
 	printf("| luas permukaan bola: %.2f \n", luas);
 	printf("=================================================\n");
-	printf("| 1. Input menghitung lagi? \n");
+	printf("| 1. Ingin menghitung lagi? \n");
 	printf("| 2. Keluar\n");
 	printf("=================================================\n");
 	printf("| Masukkan pilihan anda: ");
@@ -314,11 +292,11 @@ float luas_permukaan_bola(){
 	else {
 		system("cls");
 		printf("=================================================\n");		
-		printf("|            Masukkan input yang benar!         |\n");
+		printf("|          Program Kembali Ke Menu Awal         |\n");
 		printf("=================================================\n");	
 		getch();
 		system("cls");
-		main();
+		menu_awal();
 	}
 	return 0;
 }
@@ -330,11 +308,11 @@ float volume_bola(){
 	printf("=================================================\n");
 	printf("| Masukkan jari-jari : ");
 	r=check();
-	volume= rumus_volume_bola(r);
+	volume= 1.3*phi*r*r*r;
 	printf("=================================================\n");
 	printf("| volume bola: %.2f \n", volume);
 	printf("=================================================\n");
-	printf("| 1. Input menghitung lagi? \n");
+	printf("| 1. Ingin menghitung lagi? \n");
 	printf("| 2. Keluar\n");
 	printf("=================================================\n");
 	printf("| Masukkan pilihan anda: ");
@@ -350,28 +328,15 @@ float volume_bola(){
 	else {
 		system("cls");
 		printf("=================================================\n");		
-		printf("|            Masukkan input yang benar!         |\n");
+		printf("|          Program Kembali Ke Menu Awal         |\n");
 		printf("=================================================\n");	
 		getch();
 		system("cls");
-		main();
+		menu_awal();
 	}
 	return 0;
 }
 
-float rumus_luas_permukaan_bola(float r){
-	
-	float luas;
-	luas = 4*phi*r*r;
-	return luas;
-}
-
-float rumus_volume_bola(float r){
-	
-	float volume;
-	volume = 1.3*phi*r*r*r;
-	return volume;
-}
 
 int menu_limas_segiempat(){
 	
@@ -396,7 +361,7 @@ int menu_limas_segiempat(){
 		volume_limas_segiempat();
 	}
 	else if(opsi==3){
-		main();
+		menu_awal();
 	}
 	else if(opsi==4){
 		system("cls");
@@ -420,18 +385,20 @@ int menu_limas_segiempat(){
 
 float luas_permukaan_limas_segiempat(){
 	
-	float sisi, t, luas, opsi;
+	float sisi, t, luas, S_tegak, L_segitiga, opsi;
 	
 	printf("=================================================\n");
 	printf("| Masukkan sisi   : ");
 	sisi=check();
 	printf("| Masukkan tinggi : ");
 	t=check();
-	luas= rumus_luas_permukaan_limas_segiempat(sisi,t);
+	S_tegak =sqrt((sisi*0.5)*(sisi*0.5)+(t*t));
+	L_segitiga=0.5*sisi*S_tegak;
+	luas = (sisi*sisi)+(4*L_segitiga);
 	printf("=================================================\n");
 	printf("| luas permukaan limas segiempat: %.2f \n", luas);
 	printf("=================================================\n");
-	printf("| 1. Input menghitung lagi? \n");
+	printf("| 1. Ingin menghitung lagi? \n");
 	printf("| 2. Keluar\n");
 	printf("=================================================\n");
 	printf("| Masukkan pilihan anda: ");
@@ -447,11 +414,11 @@ float luas_permukaan_limas_segiempat(){
 	else {
 		system("cls");
 		printf("=================================================\n");		
-		printf("|            Masukkan input yang benar!         |\n");
+		printf("|          Program Kembali Ke Menu Awal         |\n");
 		printf("=================================================\n");	
 		getch();
 		system("cls");
-		main();
+		menu_awal();
 	}
 	return 0;
 }
@@ -465,11 +432,11 @@ float volume_limas_segiempat(){
 	sisi=check();
 	printf("| Masukkan tinggi : ");
 	t=check();
-	volume= rumus_volume_limas_segiempat(sisi,t);
+	volume= (0.3)*(sisi*sisi)*t;
 	printf("=================================================\n");
 	printf("| volume limas segiempat: %.2f \n", volume);
 	printf("=================================================\n");
-	printf("| 1. Input menghitung lagi? \n");
+	printf("| 1. Ingin menghitung lagi? \n");
 	printf("| 2. Keluar\n");
 	printf("=================================================\n");
 	printf("| Masukkan pilihan anda: ");
@@ -485,31 +452,16 @@ float volume_limas_segiempat(){
 	else {
 		system("cls");
 		printf("=================================================\n");		
-		printf("|            Masukkan input yang benar!         |\n");
+		printf("|          Program Kembali Ke Menu Awal         |\n");
 		printf("=================================================\n");	
 		getch();
 		system("cls");
-		main();
+		menu_awal();
 	}
 	return 0;
 }
 
-float rumus_luas_permukaan_limas_segiempat(float sisi, float t){
-	
-	float S_tegak,L_segitiga;
-	float luas;
-	S_tegak =sqrt((sisi*0.5)*(sisi*0.5)+(t*t));
-	L_segitiga=0.5*sisi*S_tegak;
-	luas = (sisi*sisi)+(4*L_segitiga);
-	return luas;
-}
 
-float rumus_volume_limas_segiempat(float sisi, float t){
-	
-	float volume;
-	volume = (0.3)*(sisi*sisi)*t;
-	return volume;
-}
 
 int menu_prisma_segitiga(){
 	
@@ -534,7 +486,7 @@ int menu_prisma_segitiga(){
 		volume_prisma_segitiga();
 	}
 	else if(opsi==3){
-		main();
+		menu_awal();
 	}
 	else if(opsi==4){
 		system("cls");
@@ -558,7 +510,7 @@ int menu_prisma_segitiga(){
 
 float luas_permukaan_prisma_segitiga(){
 	
-	float a, t, T_prisma, luas, opsi;
+	float a, t, T_prisma, S_miring, luas, opsi;
 	
 	printf("=================================================\n");
 	printf("| Masukkan alas          : ");
@@ -567,11 +519,12 @@ float luas_permukaan_prisma_segitiga(){
 	t=check();
 	printf("| Masukkan tinggi prisma : ");
 	T_prisma=check();
-	luas= rumus_luas_permukaan_prisma_segitiga(a,t,T_prisma);
+	S_miring= sqrt((a*a)+(t*t));
+	luas = (2*0.5*a*t)+((a+t+S_miring)*T_prisma);
 	printf("=================================================\n");
 	printf("| luas permukaan prisma segitiga: %.2f \n", luas);
 	printf("=================================================\n");
-	printf("| 1. Input menghitung lagi? \n");
+	printf("| 1. Ingin menghitung lagi? \n");
 	printf("| 2. Keluar\n");
 	printf("=================================================\n");
 	printf("| Masukkan pilihan anda: ");
@@ -587,11 +540,11 @@ float luas_permukaan_prisma_segitiga(){
 	else {
 		system("cls");
 		printf("=================================================\n");		
-		printf("|            Masukkan input yang benar!         |\n");
+		printf("|          Program Kembali Ke Menu Awal         |\n");
 		printf("=================================================\n");	
 		getch();
 		system("cls");
-		main();
+		menu_awal();
 	}
 	return 0;
 }
@@ -607,11 +560,11 @@ float volume_prisma_segitiga(){
 	t=check();
 	printf("| Masukkan tinggi prisma : ");
 	T_prisma=check();
-	volume= rumus_volume_prisma_segitiga(a,t,T_prisma);
+	volume= (0.5*a*t)*T_prisma;
 	printf("=================================================\n");
 	printf("\n volume prisma segitiga: %.2f \n\n", volume);
 	printf("=================================================\n");
-	printf("| 1. Input menghitung lagi? \n");
+	printf("| 1. Ingin menghitung lagi? \n");
 	printf("| 2. Keluar\n");
 	printf("=================================================\n");
 	printf("| Masukkan pilihan anda: ");
@@ -627,31 +580,15 @@ float volume_prisma_segitiga(){
 	else {
 		system("cls");
 		printf("=================================================\n");		
-		printf("|            Masukkan input yang benar!         |\n");
+		printf("|          Program Kembali Ke Menu Awal         |\n");
 		printf("=================================================\n");	
 		getch();
 		system("cls");
-		main();
+		menu_awal();
 	}
 	return 0;
 }
 
-
-float rumus_luas_permukaan_prisma_segitiga(float a, float t, float T_prisma){
-	
-	float S_miring;
-	float luas;
-	S_miring= sqrt((a*a)+(t*t));
-	luas = (2*0.5*a*t)+((a+t+S_miring)*T_prisma);
-	return luas;
-}
-
-float rumus_volume_prisma_segitiga(float a, float t, float T_prisma){
-	
-	float volume;
-	volume = (0.5*a*t)*T_prisma;
-	return volume;
-}
 
 int menu_kerucut(){
 	
@@ -676,7 +613,7 @@ int menu_kerucut(){
 		volume_kerucut();
 	}
 	else if(opsi==3){
-		main();
+		menu_awal();
 	}
 	else if(opsi==4){
 		system("cls");
@@ -700,18 +637,19 @@ int menu_kerucut(){
 
 float luas_permukaan_kerucut(){
 	
-	float r, t, luas, opsi;
+	float r, t, s, luas, opsi;
 	
 	printf("=================================================\n");
 	printf("| Masukkan jari-jari : ");
 	r=check();
 	printf("| Masukkan tinggi    : ");
 	t=check();
-	luas= rumus_luas_permukaan_kerucut(r,t);
+	s = sqrt((r*r)+(t*t));
+	luas = (phi*r)*(r+s);
 	printf("=================================================\n");
 	printf("\n luas permukaan kerucut: %.2f \n\n", luas);
 	printf("=================================================\n");
-	printf("| 1. Input menghitung lagi? \n");
+	printf("| 1. Ingin menghitung lagi? \n");
 	printf("| 2. Keluar\n");
 	printf("=================================================\n");
 	printf("| Masukkan pilihan anda: ");
@@ -727,11 +665,11 @@ float luas_permukaan_kerucut(){
 	else {
 		system("cls");
 		printf("=================================================\n");		
-		printf("|            Masukkan input yang benar!         |\n");
-		printf("=================================================\n");	
+		printf("|          Program Kembali Ke Menu Awal         |\n");
+		printf("=================================================\n");		
 		getch();
 		system("cls");
-		main();
+		menu_awal();
 	}
 	return 0;
 }
@@ -745,11 +683,11 @@ float volume_kerucut(){
 	r=check();
 	printf("| Masukkan tinggi    : ");
 	t=check();
-	volume= rumus_volume_kerucut(r,t);
+	volume= 0.3*phi*r*r*t;
 	printf("=================================================\n");
 	printf("\n volume kerucut: %.2f \n\n", volume);
 	printf("=================================================\n");
-	printf("| 1. Input menghitung lagi? \n");
+	printf("| 1. Ingin menghitung lagi? \n");
 	printf("| 2. Keluar\n");
 	printf("=================================================\n");
 	printf("| Masukkan pilihan anda: ");
@@ -765,30 +703,15 @@ float volume_kerucut(){
 	else {
 		system("cls");
 		printf("=================================================\n");		
-		printf("|            Masukkan input yang benar!         |\n");
-		printf("=================================================\n");	
+		printf("|          Program Kembali Ke Menu Awal         |\n");
+		printf("=================================================\n");		
 		getch();
 		system("cls");
-		main();
+		menu_awal();
 	}
 	return 0;
 }
 
-float rumus_luas_permukaan_kerucut(float r, float t){
-	
-	float s;
-	float luas;
-	s = sqrt((r*r)+(t*t));
-	luas = (phi*r)*(r+s);
-	return luas;
-}
-
-float rumus_volume_kerucut(float r, float t){
-	
-	float volume;
-	volume = 0.3*phi*r*r*t;
-	return volume;
-}
 
 
 double check(){
@@ -799,17 +722,15 @@ double check(){
 	cek=scanf("%lf%c", &input,&checkinput);
 	
 	if(cek!=2 || checkinput!='\n'){
-		system("cls");
-		printf("\n\n\n");
-		printf("inputan anda tidak sesuai kriteria! \n\n");
-		system("pause");
-		system("cls");
-		cek=1;
+		printf("inputan anda tidak sesuai kriteria!\n");
+		printf("Masukkan angka bukan huruf: ");
 		fflush(stdin);
-		main();
+		cek=1;
+		return check();
+		
 	}
 	else if(input<=0){
-		printf("input anda invalid! \n");
+		printf("inputan anda tidak sesuai kriteria! \n");
 		printf("Masukkan angka selain -/0 : ");
 		fflush(stdin);
 		cek=1;
@@ -836,7 +757,7 @@ int keluar(){
 	system("cls");
 	
 	if (opsi==1){
-		main();
+		menu_awal();
 	}
 	else if(opsi==2){
 		system("cls");
@@ -852,7 +773,7 @@ int keluar(){
 		printf("=================================================\n");	
 		getch();
 		system("cls");
-		main();
+		keluar();
 	}
 	
 }
